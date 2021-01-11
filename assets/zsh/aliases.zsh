281cb65eb67ead16b2fbc1c2279da6c34761b429
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2021-01-11 18:47:56 +0100

    Added zsh aliases for colored output

---
+++ assets/zsh/aliases.zsh
@@ -0,0 +1,7 @@
+alias ip='ip -color=auto'
+alias ls='ls --color=auto'
+alias diff='diff --color=auto'
+alias grep='grep --color=auto'
+
+alias moon='curl wttr.in/Moon'
+alias weather='curl wttr.in/Berlin'
