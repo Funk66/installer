7feb9b6975a20df9c9df4a64ea3facb3578ac664
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2021-02-04 19:31:55 +0100

    Various improvements

--- assets/kialo/eks.zsh
+++ assets/kialo/eks.zsh
@@ -8,7 +8,7 @@ typeset -A CLUSTERS=(
 
 CLUSTER=$1
 [ -z $CLUSTERS[$CLUSTER] ] && CLUSTER=$(echo ${(k)CLUSTERS} | tr ' ' '\n' | fzf -1 --height 9 --reverse --query=${CLUSTER})
-[[ "${KUBECONFIG}" =~ "/${CLUSTER}$" ]] && return
+[ -z $CLUSTER ] || [[ "${KUBECONFIG}" =~ "/${CLUSTER}$" ]] && return
 read ACCOUNT REGION NAMESPACE <<< $(echo $CLUSTERS[$CLUSTER])
 [ "$AWSUME_PROFILE" != "$ACCOUNT" ] && assume $ACCOUNT
 export KUBECONFIG=$KIALO_ROOT/.kube/$CLUSTER
--- assets/zsh/aliases.zsh
+++ assets/zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias ip='ip -color=auto'
 alias ls='ls --color=auto'
 alias diff='diff --color=auto'
 alias grep='grep --color=auto'
+alias d='dirs -v'
 
 alias moon='curl wttr.in/Moon'
 alias weather='curl wttr.in/Berlin'
