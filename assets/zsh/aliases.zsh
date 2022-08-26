7e7c47761a3afcbb3be47aa6bbaf4277f914ee0b
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2022-08-26 17:40:52 +0200

    Add aliases

--- assets/zsh/aliases.zsh
+++ assets/zsh/aliases.zsh
@@ -7,9 +7,16 @@ fi
 alias grep='grep --color=auto'
 alias d='dirs -v'
 
+alias isodate='date -u +"%Y-%m-%dT%H:%M:%SZ"'
 alias moon='curl wttr.in/Moon'
 alias weather='curl wttr.in/Berlin'
 
+alias otp="ykman --device 6931997 oath accounts code aws --single | pbcopy"
+
 cht() {
   curl cht.sh/$1
+}
+
+grim() {
+    vim $(rg -l "$1")
 }
