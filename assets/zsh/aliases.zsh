bba9b222ead9adc02e6cfd8025f95e5237734585
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2022-08-30 21:35:33 +0200

    Move secrets to gopass

--- assets/zsh/aliases.zsh
+++ assets/zsh/aliases.zsh
@@ -4,14 +4,13 @@ then
   alias ls='ls --color=auto'
   alias diff='diff --color=auto'
 fi
+alias python=python3
 alias grep='grep --color=auto'
 alias d='dirs -v'
 
 alias isodate='date -u +"%Y-%m-%dT%H:%M:%SZ"'
 alias moon='curl wttr.in/Moon'
 alias weather='curl wttr.in/Berlin'
-
-alias otp="ykman --device 6931997 oath accounts code aws --single | pbcopy"
 
 cht() {
   curl cht.sh/$1
